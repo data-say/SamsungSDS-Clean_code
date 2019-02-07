a4de1012ebfd8bab7dba0926b133b12cc63b06e2
byunghyunyu <byunghyun.yu@samsung.com>
2019-02-07 17:56:48 +0900

    Step33. AgedBrie 조건문 합치기.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -17,6 +17,12 @@ class GildedRose {
 				if (item.quality < 50) {
 					item.quality = item.quality + 1;
 				}
+				
+				if (item.sellIn < 1) {
+					if (item.quality < 50) {
+						item.quality = item.quality + 1;
+					}
+				}
 			} else if (item.name.equals(BACKSTAGE_PASS)) {
 				if (item.quality < 50) {
 					item.quality = item.quality + 1;
@@ -45,11 +51,7 @@ class GildedRose {
 			}
 
 			if (item.name.equals(AGED_BRIE)) {
-				if (item.sellIn < 0) {
-					if (item.quality < 50) {
-						item.quality = item.quality + 1;
-					}
-				}
+				
 			} else if (item.name.equals(BACKSTAGE_PASS)) {
 				if (item.sellIn < 0) {
 					item.quality = item.quality - item.quality;
