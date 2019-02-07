487fa3e889eb9e5ccd8d6beaa134a83f8dad347e
byunghyunyu <byunghyun.yu@samsung.com>
2019-02-07 20:35:42 +0900

    Step71. gildedRoseItem.updateQuality() 중복코드 제거.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -13,22 +13,21 @@ class GildedRose {
 	public void updateQuality() {
 		for (int i = 0; i < items.length; i++) {
 			Item item = items[i];
+			
 			GildedRoseItem gildedRoseItem;
 			
 			if (item.name.equals(AGED_BRIE)) {
 				gildedRoseItem = new AgedBrieItem(item);
-				gildedRoseItem.updateQuality();
 			} else if (item.name.equals(BACKSTAGE_PASS)) {
 				gildedRoseItem = new BackStagePassItem(item); 
-				gildedRoseItem.updateQuality();
 			} else if (item.name.equals(SULFURAS)) {
 				gildedRoseItem = new SulfurasItem(item);
-				gildedRoseItem.updateQuality(); 
 			} else {
 				gildedRoseItem = new NormalItem(item); 
-				gildedRoseItem.updateQuality();
 			}
 
+			gildedRoseItem.updateQuality();
+			
 			if (item.name.equals(SULFURAS)) {
 			} else {
 				item.sellIn = item.sellIn - 1;
