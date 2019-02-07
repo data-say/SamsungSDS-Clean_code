2ca7a7e7d340b4904d135a498c65fed430b117d2
byunghyunyu <byunghyun.yu@samsung.com>
2019-02-07 15:58:33 +0900

    Step0. 테스트 케이스 템플릿 등록

---
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,65 @@
+package com.gildedrose;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GildedRoseTest {
+
+	@Test
+	public void Step1_GildedRose_생성자_테스트() {
+		
+	}
+	
+	@Test
+	public void Step2_Normal아이템_판매기간0_품질0_테스트() {
+		
+	}
+	
+	@Test
+	public void Step3_Normal아이템_판매기간0_품질5_테스트() {
+		
+	}
+	
+	@Test
+	public void Step4_Sulfuras아이템_판매기간0_품질5_테스트() {
+		
+	}
+	
+	// Step5. 상수 리팩토링
+	
+	@Test
+	public void Step6_AgedBrie아이템_판매기간0_품질0_테스트() {
+		
+	}
+	
+	@Test
+	public void Step7_BackstagePass아이템_판매기간0_품질0_테스트() {
+		
+	}
+	
+	@Test
+	public void Step8_BackstagePass아이템_판매기간0_품질51_테스트() {
+		
+	}
+	
+	@Test
+	public void Step9_BackstagePass아이템_판매기간15_품질0_테스트() {
+		
+	}
+	
+	@Test
+	public void Step10_Sulfuras아이템_판매기간1_품질5_테스트() {
+		
+	}
+	
+	@Test
+	public void Step11_AgedBrie아이템_판매기간0_품질55_테스트() {
+		
+	}
+	
+	@Test
+	public void Step12_GildedRose_Item_toString_테스트() {
+		
+	}
+}
