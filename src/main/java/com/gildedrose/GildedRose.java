6887806f11682c4f4a93791343dafc609e8a8ea2
byunghyunyu <byunghyun.yu@samsung.com>
2019-02-07 16:57:51 +0900

    Step26. 불필요 조건 삭제.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -21,18 +21,12 @@ class GildedRose {
 				if (item.quality < 50) {
 					item.quality = item.quality + 1;
 
-					if (item.name.equals(BACKSTAGE_PASS)) {
-						if (item.sellIn < 11) {
-							if (item.quality < 50) {
-								item.quality = item.quality + 1;
-							}
-						}
+					if (item.sellIn < 11) {
+						item.quality = item.quality + 1;
+					}
 
-						if (item.sellIn < 6) {
-							if (item.quality < 50) {
-								item.quality = item.quality + 1;
-							}
-						}
+					if (item.sellIn < 6) {
+						item.quality = item.quality + 1;
 					}
 				}
 			} else {
