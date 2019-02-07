375c9bdaf6c6e76c12f39009af338ba5c4d58c14
byunghyunyu <byunghyun.yu@samsung.com>
2019-02-07 17:53:20 +0900

    Step31. 이름 비교 조건을 상위 단계로 이동.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -50,17 +50,19 @@ class GildedRose {
 						item.quality = item.quality + 1;
 					}
 				}
+			} else if (item.name.equals(BACKSTAGE_PASS)) {
+				if (item.sellIn < 0) {
+					item.quality = item.quality - item.quality;
+				}
+			} else if (item.name.equals(SULFURAS)) {
+				if (item.sellIn < 0) {
+					if (item.quality > 0) {
+					}
+				}
 			} else {
 				if (item.sellIn < 0) {
-					if (item.name.equals(BACKSTAGE_PASS)) {
-						item.quality = item.quality - item.quality;
-					} else {
-						if (item.quality > 0) {
-							if (item.name.equals(SULFURAS)) {
-							} else {
-								item.quality = item.quality - 1;
-							}
-						}
+					if (item.quality > 0) {
+						item.quality = item.quality - 1;
 					}
 				}
 			}
