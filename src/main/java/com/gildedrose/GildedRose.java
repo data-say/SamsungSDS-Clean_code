1c7f13f84a1d0513fd517ff5d0582e299947a7ee
byunghyunyu <byunghyun.yu@samsung.com>
2019-02-07 17:59:04 +0900

    Step36. NormalItem 조건문 합치기.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -52,6 +52,12 @@ class GildedRose {
 				if (item.quality > 0) {
 					item.quality = item.quality - 1;
 				}
+				
+				if (item.sellIn < 1) {
+					if (item.quality > 0) {
+						item.quality = item.quality - 1;
+					}
+				}
 			}
 
 			if (item.name.equals(SULFURAS)) {
@@ -66,11 +72,7 @@ class GildedRose {
 			} else if (item.name.equals(SULFURAS)) {
 				
 			} else {
-				if (item.sellIn < 0) {
-					if (item.quality > 0) {
-						item.quality = item.quality - 1;
-					}
-				}
+				
 			}
 		}
 	}
