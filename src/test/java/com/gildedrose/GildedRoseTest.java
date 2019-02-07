238fc42d11b637362e117108faa946fd85c583b3
byunghyunyu <byunghyun.yu@samsung.com>
2019-02-07 16:02:16 +0900

    Step4. Sulfuras아이템 판매기간0 품질5 테스트.

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -54,6 +54,17 @@ public class GildedRoseTest {
 	@Test
 	public void Step4_Sulfuras아이템_판매기간0_품질5_테스트() {
 		
+		// arrange
+		Item[] items= new Item[]{new Item("Sulfuras, Hand of Ragnaros", 0, 5)};
+		GildedRose gildedRose= new GildedRose(items);
+		
+		// act
+		gildedRose.updateQuality();
+		
+		// assert
+		assertEquals("Sulfuras, Hand of Ragnaros", items[0].name);
+		assertEquals(0, items[0].sellIn);
+		assertEquals(5, items[0].quality);		
 	}
 	
 	// Step5. 상수 리팩토링
