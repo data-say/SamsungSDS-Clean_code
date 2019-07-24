a7660a0ab21865e65a3b95f98470cd0c6bab4d20
nubang <nubang7@gmail.com>
2019-07-24 14:46:05 +0900

    대학생 멘토링 TC 100% 완성

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -102,7 +102,17 @@ public class GildedRoseTest {
 
 	@Test
 	public void Step7_AgedBrie아이템_판매기간0_품질50_테스트() {
-		
+		// arrange
+		Item[] items = new Item[] { new Item("Aged Brie", 0, 50) };
+		GildedRose gildedRose = new GildedRose(items);
+
+		// act
+		gildedRose.updateQuality();
+
+		// assert
+		assertEquals("Aged Brie", items[0].name);
+		assertEquals(-1, items[0].sellIn);
+		assertEquals(50, items[0].quality);
 	}
 
 	@Test
@@ -123,26 +133,91 @@ public class GildedRoseTest {
 
 	@Test
 	public void Step9_BackStagePass아이템_판매기간10_품질49_테스트() {
-		
+		// arrange
+		Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49) };
+		GildedRose gildedRose = new GildedRose(items);
+
+		// act
+		gildedRose.updateQuality();
+
+		// assert
+		assertEquals("Backstage passes to a TAFKAL80ETC concert", items[0].name);
+		assertEquals(9, items[0].sellIn);
+		assertEquals(50, items[0].quality);
 	}
 
 	@Test
 	public void Step10_BackStagePass아이템_판매기간11_품질49_테스트() {
+		// arrange
+		Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 11, 49) };
+		GildedRose gildedRose = new GildedRose(items);
 
+		// act
+		gildedRose.updateQuality();
+
+		// assert
+		assertEquals("Backstage passes to a TAFKAL80ETC concert", items[0].name);
+		assertEquals(10, items[0].sellIn);
+		assertEquals(50, items[0].quality);
 	}
 
 	@Test
-	public void Step11_XXXXX아이템_판매기간X_품질X_테스트() {
+	public void Step11_BackStagePass아이템_판매기간5_품질49_테스트() {
+		// arrange
+		Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49) };
+		GildedRose gildedRose = new GildedRose(items);
 
+		// act
+		gildedRose.updateQuality();
+
+		// assert
+		assertEquals("Backstage passes to a TAFKAL80ETC concert", items[0].name);
+		assertEquals(4, items[0].sellIn);
+		assertEquals(50, items[0].quality);
 	}
 
 	@Test
-	public void Step12_XXXXX아이템_판매기간X_품질X_테스트() {
+	public void Step12_Normal아이템_판매기간0_품질2_테스트() {
+		// arrange
+		Item[] items = new Item[] { new Item("Normal Item", 0, 2) };
+		GildedRose gildedRose = new GildedRose(items);
 
+		// act
+		gildedRose.updateQuality();
+
+		// assert
+		assertEquals("Normal Item", items[0].name);
+		assertEquals(-1, items[0].sellIn);
+		assertEquals(0, items[0].quality);
 	}
 
 	@Test
-	public void Step13_XXXXX아이템_판매기간X_품질X_테스트() {
+	public void Step13_Sulfuras아이템_판매기간_마이너스1_품질2_테스트() {
+		// arrange
+		Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", -1, 2) };
+		GildedRose gildedRose = new GildedRose(items);
 
+		// act
+		gildedRose.updateQuality();
+
+		// assert
+		assertEquals("Sulfuras, Hand of Ragnaros", items[0].name);
+		assertEquals(-1, items[0].sellIn);
+		assertEquals(2, items[0].quality);
+	}
+	
+	@Test
+	public void Step13_AgedBrie아이템_판매기간_마이너스1_품질2_테스트() {
+		// arrange
+		Item[] items = new Item[] { new Item("Aged Brie", -1, 2) };
+		GildedRose gildedRose = new GildedRose(items);
+
+		// act
+		gildedRose.updateQuality();
+
+		// assert
+		assertEquals("Aged Brie", items[0].name);
+		assertEquals(-2, items[0].sellIn);
+		assertEquals(4, items[0].quality);
 	}
 }
